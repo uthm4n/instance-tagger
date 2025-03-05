5fcf900fbd21f1916306ef9e1c5e97f2fbac32de
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2025-03-05 12:39:49 +0000

    Update TagRetriever.groovy

--- TagRetriever.groovy
+++ TagRetriever.groovy
@@ -3,51 +3,51 @@ package com.morpheus
 import org.slf4j.*
 import groovy.util.logging.Slf4j
 import com.morpheus.*
-import grails.util.Holders
+import com.morpheusdata.core.util.HttpApiClient
+import com.morpheusdata.core.RequestOptions
 import groovy.json.JsonOutput
 import java.time.LocalDateTime
-import java.util.Set
 import java.time.format.DateTimeFormatter
- 
-serverId = server.id ?: instance.container?.serverId
- 
-def opts = [server: []]
- 
+
 def logWithTimestamp = { message ->
     def timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
     println "${timestamp} - ${message}"
 }
 
-logWithTimestamp("INSTANCE TAGS:\r\n${instance.tags}\r\n")
- 
-ComputeServer.withNewSession { session ->
-    opts.server = ComputeServer.get(serverId)
-    def serverTags = opts.server.getTags()
-    logWithTimestamp("SERVER TAGS:\r\n${serverTags}\r\n")
-
-  //  def updatedInterfaces = updateResults.toSet()
-  //  opts.server.setInterfaces(updatedInterfaces)
- 
-  //  opts.server.save(flush: true, failOnError: true
-  logWithTimestamp("SERVER DUMP:\r\n${opts.server.dump()}")
-
+if (!instance?.tags) {
+    logWithTimestamp("INSTANCE TAGS: No tags found.\r\n")
+} else {
+    logWithTimestamp("INSTANCE TAGS:\r\n${JsonOutput.prettyPrint(JsonOutput.toJson(instance.tags))}\r\n")
 }
- 
-@Slf4j
-class TagRetriever {
-    private grailsApplication
-    private mainContext
-    private serverService
-    private morpheusComputeService
- 
-    public TagRetriever() {
-        this.grailsApplication = Holders.grailsApplication
-        this.mainContext = Holders.grailsApplication.mainContext
-        this.serverService = this.mainContext["serverService"]
-        this.morpheusComputeService = this.mainContext["morpheusComputeService"]
-    }
- 
-    def getServerTags() {
-        return null
+
+def morpheusUrl = morpheus.applianceUrl
+def apiToken = morpheus.getApiAccessToken()
+
+def getInstancesWithMissingTags(String missingTag) {
+    HttpApiClient client = new HttpApiClient()
+    RequestOptions requestOptions = new RequestOptions()
+    
+    requestOptions.headers = ["Authorization": "Bearer ${apiToken}"]
+
+    def instanceApi = client.callJsonApi(morpheusUrl, "/api/instances?max=1000", null, null, requestOptions, "GET")
+    
+    if (instanceApi.success) {
+        def instances = instanceApi.data.instances ?: []
+        logWithTimestamp("Total instances retrieved: ${instances.size()}")
+
+        def instancesWithoutTag = instances.findAll { instance ->
+            def tags = instance.tags?.collect { it.name } ?: []
+            return tags.isEmpty() || !tags.contains(missingTag)
+        }.collect { it.id } 
+
+        logWithTimestamp("Total instances missing tag '${missingTag}': ${instancesWithoutTag.size()}")
+        return instancesWithoutTag
+    } else {
+        logWithTimestamp("Error fetching instances: ${instanceApi.errorMessage}")
+        return []
     }
 }
+
+def missingTag = "Uthman"
+def instancesToUpdate = getInstancesWithMissingTags(missingTag)
+logWithTimestamp("INSTANCES WITHOUT REQUIRED TAG '${missingTag}':\r\n${instancesToUpdate}\r\n")
